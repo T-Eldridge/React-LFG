f2f3c4a7b65de6f9d277d2ab5842d6eebe85b1a8
TEldridge <skippydrumskins@yahoo.com>
2020-12-08 14:17:58 -0600

    added seachbar to dashboard

--- src/components/dasboard.js
+++ src/components/dasboard.js
@@ -6,16 +6,12 @@ import {
     TableCell,
     TableBody,
  } from '@material-ui/core';
- import searchForm from "../components/searchForm"
+ import App from "../components/searchForm"
 
 
 
 class Dashboard extends Component {
-    componentDidMount() {
-        // if mounts display favorite games
-        // this.props.getUserFavorites(this.props.favorites) 
-    }
-
+   
     render() {
         return (
             <div>
@@ -24,20 +20,20 @@ class Dashboard extends Component {
                         <TableHead>
                            <h1> {this.props.userName}Favorite Games</h1> 
                         </TableHead>
-                         <searchForm />
                         <TableBody>
                             <TableCell>
                                 <TableRow align="left">
                                     <p style={{ paddingLeft: 60 }}>
                                        <b>Favorite Games</b>{" "}
                                        <b style={{ align: "right "}}>
-                                           :${this.props.favorites}
+                                           :{this.props.favorites}
                                        </b>
                                     </p>
                                 </TableRow>
                             </TableCell>
                         </TableBody>
                     </Table>
+                    <App />
                 </div>
             </div>
         )
