bebaa0bb3b0085d696caa90cc5732c74b6fe93fa
TEldridge <skippydrumskins@yahoo.com>
2020-12-08 13:02:57 -0600

    cleared redudant files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Router from './router';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './App.css';
 import store from './redux/store';
-import Navigation from './containers/navigation'
+import HandleLogin from './containers/handlelogin';
+import { Button, AppBar, Toolbar } from "@material-ui/core";
 
 
-function App() {
-  return (
-    <div className="container">
-  
+class App extends Component {
+  render () {
+    return (
       <Provider store={store}>
-        <BrowserRouter>
-        <Navigation /> 
-        <Router />
-        </BrowserRouter>
-      </Provider>
-   
+      <div className="container">
+      <BrowserRouter>
+      <AppBar style={{ background: "blue" }} position="static">
+        <Toolbar>
+          <h1>Looking For Games</h1>
+        </Toolbar>
+        <div align="center">
+          <a href="/">
+            <Button>Home</Button>
+          </a>
+        <HandleLogin />
+        </div>
+      </AppBar>
+      <Router />
+      </BrowserRouter>
     </div>
-  );
+    </Provider>
+    )
+  }
 }
 
 export default App;
